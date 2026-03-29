8f8860030fedd92e83789db9fb8891ddb54224eb
Manav <manavtarashnayak@gmail.com>
2026-03-30 00:29:41 +0530

    UC5 done

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -3,20 +3,15 @@ package org.example;
 public class HelloApp {
     public static void main(String[] args) {
 
+        // Default case
         if (args.length == 0) {
             System.out.println("Hello, World!");
-        } else {
-            System.out.print("Hello, ");
-            
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i]);
-                
-                if (i < args.length - 1) {
-                    System.out.print(", ");
-                }
-            }
-            
-            System.out.println("!");
+            return;
+        }
+
+        // Enhanced for loop (for-each)
+        for (String name : args) {
+            System.out.println("Hello, " + name + "!");
         }
     }
 }
